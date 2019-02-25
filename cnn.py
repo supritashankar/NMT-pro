1472fccdfacfceeda7e1a021fc8a36666301d8be
Suprita Shankar <suprita.shankar@gmail.com>
2019-02-24 16:12:34 -0800

    update cnn and highway

--- cnn.py
+++ cnn.py
@@ -8,23 +8,85 @@ CS224N 2018-19: Homework 5
 ### YOUR CODE HERE for part 1i
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import numpy as np
 
 class CNN(nn.Module):
     """
+    CNN Network
     """
     def __init__(self, output_features, char_embeddings=50, kernel_size=5):
         super(CNN, self).__init__()
         m_word = 21
+        self.after_cnn = None
+        self.after_relu = None
         self.cnn = nn.Conv1d(in_channels=char_embeddings, out_channels=output_features, kernel_size=kernel_size, bias=True)
         self.maxpool = nn.MaxPool1d(m_word - kernel_size+1)
         self.relu = nn.ReLU()
 
     def forward(self, input):
-        x = self.cnn(input)
-        x = self.relu(x)
-        return self.maxpool(x)
+        """
+        :param input: - x_reshaped of dim (e_char, m_word)
+        :return: x_conv_out of dim (e_word)
+        """
+        self.after_cnn = self.cnn(input)
+        self.after_relu = self.relu(self.after_cnn)
+        return self.maxpool(self.after_relu)
 
 ### END YOUR CODE
 
+def test_cnn_network():
+    EMBED_SIZE = 2
+    INPUT_DIM = 3
+    m_word = 21
+    char_embedding = 50
+    output_features = 3
+    kernel_size = 5
+    BATCH_SIZE = 1
+    cnn = CNN(char_embeddings=50, output_features=output_features, kernel_size=5)
+    ## Check input and output dims
+    input = torch.randn(BATCH_SIZE, char_embedding, m_word)
+    output = cnn.forward(input)
+    assert input.size() == torch.Size([BATCH_SIZE, char_embedding, m_word])
+    assert output.size() == torch.Size([BATCH_SIZE, output_features, 1])
+
+    ## Check intermediate dims
+    assert cnn.after_cnn.size() == torch.Size([BATCH_SIZE, output_features, m_word-kernel_size+1])
+    assert cnn.after_relu.size() == torch.Size([BATCH_SIZE, output_features, m_word-kernel_size+1])
+
+    ## Compute the values using numpy (using np.convolve) to cross check.
+    """
+        1. Retrieve the kernel weights, bias and inputs from the above cnn
+        2. for out_channel in out_channels:
+            result = np.zeros((1, in_length-kernel_size+1))  -> create an empty result
+            for in_channel in range(in_channels):
+                result += convolve(in_channel[:,in_channel], kernel[out_channel][in_channel])
+            output[:,out_channel] = result+bias[out_channel]
+        3. Compare `output.T` against the `after_cnn` from above
+        4. Similarly for relu - we take np.maximum(output, 0)
+    """
+    in_channels = char_embedding
+    out_channels = output_features
+    in_length = m_word
+
+
+    ###################
+    # Retrieve weights, inputs, bias and compute Conv1d using numpy convolve
+    kernel  = cnn.cnn.weight.detach().numpy()
+    ip_np = input.detach().numpy()[0].T
+    bias = cnn.cnn.bias.detach().numpy()
+    output = np.empty((in_length-kernel_size+1, out_channels))
+    # output_maxpool = np.empty((1, out_channels))
+    for out_channel in range(out_channels):
+        result = np.zeros((1, in_length-kernel_size+1))
+        for in_channel in range(in_channels):
+            result += np.convolve(ip_np[:,in_channel], np.flip(kernel[out_channel][in_channel]), mode='valid')
+        output[:,out_channel] = result+bias[out_channel]
+    final_output = output.T
+    cnn_output = cnn.after_cnn.squeeze().detach().numpy()
+
+    assert (np.all(np.allclose(final_output, cnn_output))) == True
+    # after RELU
+    output_after_relu = np.maximum(output, 0)
+    assert (np.all(np.allclose(output_after_relu.T, cnn.after_relu.squeeze().detach().numpy()))) == True
+
+# test_cnn_network
--- highway.py
+++ highway.py
@@ -23,67 +23,92 @@ class Highway(nn.Module):
             self.inter_x_gate is a linear layer and has dimensions (e_word, e_word) with bias of dim (e_word)
         """
         super(Highway, self).__init__()
+        self.x_projection = None
+        self.x_gate = None
+        self.x_highway = None
+        self.x_word_emb = None
+        self.x_projection_relu = None
+        self.x_gate_sigmoid = None
         self.dropout_prob = 0.3
         self.inter_x_projection = nn.Linear(word_embedding_size, word_embedding_size, bias=True)
         self.inter_x_gate = nn.Linear(word_embedding_size, word_embedding_size, bias=True)
         self.relu_fn = nn.ReLU()
         self.sigmoid_fn = nn.Sigmoid()
         self.dropout = nn.Dropout(self.dropout_prob)
-    
+
     def forward(self, input):
         """
             params:
-                x_conv_out - of dimension .
+                x_conv_out - of dimension e_word
             return:
-                x_word_emb of dimension ()
-            
+                x_word_emb of dimension e_word
+
         """
-        # print ("highway input", x_conv_out)
-        x1 = self.inter_x_projection(input)
-        x_projection = self.relu_fn(x1)
-        x2 = self.inter_x_gate(input)
-        x_gate = self.sigmoid_fn(x2)
-        # print ("x_gate", self.x_gate.size())
-        # print ("x projection", self.x_projection.size())
-        self.x_highway = (x_gate * x_projection) + (1 - x_gate) * input
-        #print ("x highway", self.x_highway.size())
+        self.x_projection = self.inter_x_projection(input)
+        self.x_projection_relu = self.relu_fn(self.x_projection)
+        self.x_gate = self.inter_x_gate(input)
+        self.x_gate_sigmoid = self.sigmoid_fn(self.x_gate)
+        self.x_highway = (self.x_gate_sigmoid * self.x_projection_relu) + (1 - self.x_gate_sigmoid) * input
         self.x_word_emb = self.dropout(self.x_highway)
-        #print ("After dropout", self.x_word_emb)
         return self.x_word_emb
-
-
-# def sigmoid(x, derivative=False):
-#     sigm = 1. / (1. + np.exp(-x))
-#     if derivative:
-#         return sigm * (1. - sigm)
-#     return sigm   
-    
-# def test_highway_network():
-#     # TODO: try a bigger embed size
-#     # TODO: check the dimensions of weights and other things
-#     EMBED_SIZE = 2
-#     BATCH_SIZE = 3
-#     h = Highway(word_embedding_size=EMBED_SIZE)
-    
-#     x_conv_out = torch.randn((1,2))
-#     out = h.forward(x_conv_out)
-#     print("Y", h.x_highway)
-    
-#     W_proj = h.inter_x_projection.weight.detach().numpy()
-#     B_proj = h.inter_x_projection.bias.detach().numpy()
-#     W_gate = h.inter_x_gate.weight.detach().numpy()
-#     B_gate = h.inter_x_gate.bias.detach().numpy()
-#     highway_input = x_conv_out.numpy()[0]
-    
- 
-#     Y_proj = np.maximum(np.dot(W_proj, highway_input)+ B_proj,0)
-#     Y_gate = sigmoid(np.dot(W_gate, highway_input)+B_gate)
-
-#     Y_ref = np.multiply(Y_gate, Y_proj) + (1 - Y_gate) * highway_input
-#     print("Y_ref", Y_ref)
-
-    
-# test_highway_network()
     
 ### END YOUR CODE 
+
+
+"""
+Unit tests for Highway network
+"""
+
+def sigmoid(x, derivative=False):
+    sigm = 1. / (1. + np.exp(-x))
+    if derivative:
+        return sigm * (1. - sigm)
+    return sigm
+
+def test_highway_network():
+    EMBED_SIZE = 2
+    h = Highway(word_embedding_size=EMBED_SIZE)
+    INPUT_DIM = 3
+    x_conv_out = torch.randn((INPUT_DIM, EMBED_SIZE))
+    out = h.forward(x_conv_out)
+    ## Check input and output dimensions
+    assert x_conv_out.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+    assert out.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+
+    ## Check dimensions of intermediate outputs
+    assert h.x_projection.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+    assert h.x_gate.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+    assert h.x_highway.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+    assert h.x_word_emb.size() == torch.Size([INPUT_DIM, EMBED_SIZE])
+
+    ## Compute the values using numpy to crosscheck again the returned values.
+    W_proj = h.inter_x_projection.weight.detach().numpy() # Convert inter_x_projection.weight to numpy
+    B_proj = h.inter_x_projection.bias.detach().numpy() # Convert inter_x_projection.bias to numpy
+    W_gate = h.inter_x_gate.weight.detach().numpy() # Convert inter_x_gate.weight to numpy
+    B_gate = h.inter_x_gate.bias.detach().numpy() # Convert inter_x_gate.bias to numpy
+    y_proj = np.array([])
+    y_gate = np.array([])
+    y_ref = np.array([])
+
+    # for each input dimension compute:
+    #  y_proj (to be compared with x_projection)
+    #  y_gate - to be compared with x_gate
+    #  y_ref to be computed with x_highway
+    for i in range(INPUT_DIM):
+        highway_input = x_conv_out.numpy()[i]
+        y_proj_ = np.maximum(np.dot(W_proj, highway_input)+ B_proj,0)
+        y_gate_ = sigmoid(np.dot(W_gate, highway_input)+B_gate)
+        y_proj = np.append(y_proj, y_proj_)
+        y_gate = np.append(y_gate, y_gate_)
+        y_ref = np.append(y_ref, np.multiply(y_gate_, y_proj_) + (1 - y_gate_) * highway_input)
+
+    x_projection_relu = h.x_projection_relu.detach().numpy()
+    x_gate_sigmoid = h.x_gate_sigmoid.detach().numpy()
+    x_highway = h.x_highway.detach().numpy()
+    assert np.all(np.array_equal(y_proj, x_projection_relu.astype(np.float32).flatten()))
+    assert np.all(np.array_equal(y_gate, x_gate_sigmoid.astype(np.float32).flatten()))
+    assert np.all(np.array_equal(y_ref, x_highway.astype(np.float32).flatten()))
+    print ("All tests passed!")
+
+# test_highway_network()
 
